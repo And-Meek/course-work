ddaccd8f87e96af5fe70fa3ea30cd7a3e22f550d
Andrew Mikanev <i@andmeek.ru>
2024-06-26 01:50:34 +0300

    Реализовать пагинацию на Главной

--- my-app/src/bff/api/get-posts.js
+++ my-app/src/bff/api/get-posts.js
@@ -1,7 +1,12 @@
 import { HOST, PORT } from '../../constants';
 import { transformPost } from '../transformers';
 
-export const getPosts = () =>
-	fetch(`${HOST}:${PORT}/posts`)
-		.then((loadedPosts) => loadedPosts.json())
-		.then((loadedPosts) => loadedPosts && loadedPosts.map(transformPost));
+export const getPosts = (page, limit) =>
+	fetch(`${HOST}:${PORT}/posts?_page=${page}&_limit=${limit}`)
+		.then((loadedPosts) =>
+			Promise.all([loadedPosts.json(), loadedPosts.headers.get('Link')]),
+		)
+		.then(([loadedPosts, links]) => ({
+			posts: loadedPosts && loadedPosts.map(transformPost),
+			links,
+		}));
